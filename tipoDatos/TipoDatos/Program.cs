c70fa2769cfdd45f7e788cb156ef3ec48fe38072
brian-emarquez <brian3marquez@gmail.com>
2026-03-18 15:13:51 -0500

    Tipo de datos

--- tipoDatos/TipoDatos/Program.cs
+++ tipoDatos/TipoDatos/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// ===============================================
+//  TEMA 1: Variables, tipos de datos y operadores
+// ===============================================
+
+
+string nombre = "Brian";
+int edad = 30;
+double altura = 1.75;
+bool estudia = true;
+
+
+Console.WriteLine("el nombes es: " + nombre);
+Console.WriteLine("el nombes es: " + edad);
+Console.WriteLine("el nombes es: " + nombre);
+Console.WriteLine("el nombes es: " + estudia);
+

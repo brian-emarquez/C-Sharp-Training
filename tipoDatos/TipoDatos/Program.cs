50df032a9ea02efed4071f5b5368fa10b52658e4
brian-emarquez <brian3marquez@gmail.com>
2026-03-18 15:31:29 -0500

    Tipo de datos interpolacion

--- tipoDatos/TipoDatos/Program.cs
+++ tipoDatos/TipoDatos/Program.cs
@@ -19,7 +19,7 @@ Console.WriteLine("el nombes es: " + estudia);
 int a = 10;
 int b = 3;
 
-Console.WriteLine("\n--------- Operadores aritméticos ---------");
+Console.WriteLine("\n--------- Operadores aritméticos 0 ---------");
 Console.WriteLine("Suma:        " + (a + b));   // 13
 Console.WriteLine("Resta:       " + (a - b));   // 7
 Console.WriteLine("Multiplicar: " + (a * b));   // 30
@@ -32,7 +32,17 @@ double resultado = (double) a / b;               // cast a double
 Console.WriteLine("----> División real: " + resultado); // 3.333...
 
 // --- Operadores de comparación (devuelven bool) ---
-Console.WriteLine("\n------------- Comparaciones ------------------");
+Console.WriteLine("\n------------- Comparaciones 1 ------------------");
 Console.WriteLine("¿a > b?  " + (a > b));    // True
 Console.WriteLine("¿a == b? " + (a == b));   // False
 Console.WriteLine("¿a != b? " + (a != b));   // True
+
+
+// --- Interpolación de strings (forma moderna) ---
+Console.WriteLine("\n---_____________Interpolación ----------------");
+Console.WriteLine($"{nombre} tiene {edad} años y mide {altura} m.");
+
+// --- var: el compilador infiere el tipo ---
+var ciudad = "Arequipa";   // C# sabe que es string
+var año = 2026;         // C# sabe que es int
+Console.WriteLine($"Ciudad: {ciudad}, Año: {año}");
